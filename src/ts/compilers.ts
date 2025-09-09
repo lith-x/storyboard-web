a77e75a422b8551f3a4c84212bc0b49c7aa90f0e
Aaron <2858172+lith-x@users.noreply.github.com>
2025-09-09 00:02:47 -0500

    start the osb import logic

--- src/ts/compilers.ts
+++ src/ts/compilers.ts
@@ -6,42 +6,57 @@ TODO: figure out data layout for internal representation.
 TODO: don't forget z-indexing when making the objects, defined by the order
       declared in osb file, first = back, last = front.
 */
-
-type FloatArrayConstructor = Float32ArrayConstructor;
-type IntArrayConstructor = Int8ArrayConstructor | Int16ArrayConstructor | Int32ArrayConstructor | BigInt64ArrayConstructor | Uint8ArrayConstructor | Uint8ClampedArrayConstructor;
-type TypedArrayConstructor = FloatArrayConstructor | IntArrayConstructor;
-
-const handleOsbCommand = (lineVals: string[]) => {
-    const command = lineVals[0].slice(1);
-    const easing = parseInt(lineVals[1]) as Easing;
-    const startTime = parseInt(lineVals[2]); // TODO: can time be a float?
-    const endTime = parseInt(lineVals[3]);
-    
+const getVariables = (filepath: string, lines: string[], varLineIdx: number) => {
+    // TODO: are blank lines allowed in this section?
+    // line: $variable=anystring
+    const vMap = new Map<string, string>();
+    let idx = varLineIdx;
+    while (idx += 1) {
+        const [varName, varVal] = lines[idx].split("=", 2);
+        if (!varName.startsWith("$")) throw new Error(`${filepath}:${idx + 1}:1: variable name must start with $`);
+        vMap.set(varName, varVal);
+    }
+    return vMap;
 };
 
-const getVariables = (lines: string[]) => {};
+/**
+ * @returns Text of .osb with variable constants applied, and `[Variables]` section gone.
+ */
+const applyVariables = (fileText: string) => {
+    const varsIdx = fileText.search(/^\[Variables\]$/m);
+    if (varsIdx == -1) return fileText;
+    const eventsIdx = fileText.search(/^\[Events\]$/m);
+    if (eventsIdx == -1) return "";
+    // NOTE: eventsIdx + 9 assumes \n line endings, if switched to \r\n, it will be off by 1.
+    const [varsText, newFileText] = eventsIdx > varsIdx ?
+        [fileText.substring(varsIdx, eventsIdx), fileText.substring(eventsIdx)] :
+        [fileText.substring(varsIdx), fileText.substring(eventsIdx + 9, varsIdx)]
+    let result = newFileText;
+    for (const [, key, val] of varsText.matchAll(/^(\$.+)=(.+)$/gm)) {
+        result = result.replaceAll(key, val);
+    }
+    return result;
+    // TODO/optimization: compile all vars to a map, get a regex that rounds
+    //                    up all the $vars, use map to replace in one pass
+};
 
-export const osbToIr = async (filepath: string) => {
-    const fileText = await (await fetch(filepath)).text();
-    const fileLines = fileText.replaceAll("\r", "").split("\n");
-    let sb: Storyboard = {
+const osbToIr = async (filepath: string) => {
+    const fileText = (await (await fetch(filepath)).text()).replaceAll("\r", "");
+    const refinedFileText = applyVariables(fileText);
+    let lineIdx = 0;
+    const lines = refinedFileText.split("\n");
+    const linesLen = lines.length;
+    const sb: Storyboard = {
         objects: new Map(),
         timeline: []
     };
-    for (let i = 0, len = fileLines.length; i < len; i++) {
-        const line = fileLines[i];
-        if (line.startsWith("//")
-            || line.length == 0
-            || line == "[Events]") continue;
 
-        const lineVals = line.split(",");
-        switch (lineVals[0]) {
-            case "Sprite": { } break;
-            case "Animation": { } break;
-            case "Sample": { } break;
-            default: {
-
-            } break;
+    do {
+        const line = lines[lineIdx].split(",");
+        if (line[0].startsWith("//"))
+            continue;
+        switch (line[0]) {
+            
         }
-    }
+    } while ((lineIdx += 1) < linesLen)
 };
