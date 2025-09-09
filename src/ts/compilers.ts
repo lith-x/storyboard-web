c623bead78d1e6db25a3a3618cc28fc109d2bf84
Aaron <2858172+lith-x@users.noreply.github.com>
2025-09-09 05:02:09 -0500

    deciding to do typed arrays

--- src/ts/compilers.ts
+++ src/ts/compilers.ts
@@ -1,22 +1,121 @@
-import { Easing, type Storyboard, type StoryboardObject } from "./internalData";
+import {
+    CommandType, CommandTypeMap, Easing, EasingMap, Layer, LayerMap,
+    Origin, OriginMap, SbObjectType, type Command, type Storyboard,
+    type StoryboardObject, type TimelineEntry
+} from "./internalData";
 
 /*
-TODO: figure out data layout for internal representation.
-
 TODO: don't forget z-indexing when making the objects, defined by the order
       declared in osb file, first = back, last = front.
 */
-const getVariables = (filepath: string, lines: string[], varLineIdx: number) => {
-    // TODO: are blank lines allowed in this section?
-    // line: $variable=anystring
-    const vMap = new Map<string, string>();
-    let idx = varLineIdx;
-    while (idx += 1) {
-        const [varName, varVal] = lines[idx].split("=", 2);
-        if (!varName.startsWith("$")) throw new Error(`${filepath}:${idx + 1}:1: variable name must start with $`);
-        vMap.set(varName, varVal);
+
+const lineToSprite = (line: string[]) => {
+    // TODO: error checking
+    const layer = LayerMap[line[1]] ? LayerMap[line[1]] : parseInt(line[1]) as Layer;
+    const origin = OriginMap[line[2]] ? OriginMap[line[2]] : parseInt(line[2]) as Origin;
+    return {
+        id: crypto.randomUUID(),
+        type: SbObjectType.Sprite,
+        layer,
+        origin,
+        filepath: line[3],
+        initialPosition: { x: parseInt(line[4]), y: parseInt(line[5]) },
+        frameCount: null,
+        frameDelay: null,
+        loopType: null,
+        volume: null,
+        playTime: null
+    } as StoryboardObject;
+}
+
+const lineToCommand = (line: string[], id: string) => {
+    const cmdType = CommandTypeMap[line[0].substring(1)];
+    // TODO: figure out what enums can be represented as strings, and
+    //       which MUST be numeric. pretty sure easing MUST be numeric
+    //       but not super sure, so leaving this in.
+    const easing = EasingMap[line[1]] ? EasingMap[line[1]] : parseInt(line[1]) as Easing;
+    const startTime = parseInt(line[2]);
+    const endTime = parseInt(line[3]);
+    const baseCommand = {
+        type: cmdType,
+        easing: EasingMap[line[1]] ? EasingMap[line[1]] : parseInt(line[1]) as Easing,
+        startTime: parseInt(line[2]),
+        endTime: parseInt(line[3]),
+        startOpacity: undefined, endOpacity: undefined,
+        startX: undefined, startY: undefined, endX: undefined, endY: undefined,
+
+    } as Command;
+    let cmdParams;
+    switch (cmdType) {
+        case CommandType.Fade: {
+            cmdParams = {
+                startOpacity: parseFloat(line[4]),
+                endOpacity: parseFloat(line[5])
+            } as Omit<FadeCommand, keyof BaseCommand>;
+        } break;
+        case CommandType.Move: {
+            cmdParams = {
+                startX: parseInt(line[4]),
+                startY: parseInt(line[5]),
+                endX: parseInt(line[6]),
+                endY: parseInt(line[7])
+            } as Omit<MoveCommand, keyof BaseCommand>;
+        } break;
+        case CommandType.MoveX: {
+            cmdParams = {
+                startX: parseInt(line[4]),
+                endX: parseInt(line[5])
+            } as Omit<MoveXCommand, keyof BaseCommand>;
+        } break;
+        case CommandType.MoveY: {
+            cmdParams = {
+                startY: parseInt(line[4]),
+                endY: parseInt(line[5])
+            } as Omit<MoveYCommand, keyof BaseCommand>;
+        } break;
+        case CommandType.Scale: {
+            cmdParams = {
+                startScale: parseInt(line[4]),
+                endScale: parseInt(line[5])
+            } as Omit<ScaleCommand, keyof BaseCommand>;
+        } break;
+        case CommandType.VectorScale: {
+            cmdParams = {
+                startScaleX: parseFloat(line[4]),
+                startScaleY: parseFloat(line[5]),
+                endScaleX: parseFloat(line[6]),
+                endScaleY: parseFloat(line[7])
+            } as Omit<VectorScaleCommand, keyof BaseCommand>;
+        } break;
+        case CommandType.Rotate: {
+            cmdParams = {
+                startRotation: parseFloat(line[4]),
+                endRotation: parseFloat(line[5])
+            } as Omit<RotateCommand, keyof BaseCommand>;
+        } break;
+        case CommandType.Color: {
+            cmdParams = {
+                startR: parseInt(line[4]),
+                startG: parseInt(line[5]),
+                startB: parseInt(line[6]),
+                endR: parseInt(line[7]),
+                endG: parseInt(line[8]),
+                endB: parseInt(line[9])
+            } as Omit<ColorCommand, keyof BaseCommand>;
+        } break;
+        case CommandType.Parameter: {
+            cmdParams = {
+                parameter: line[4]
+            } as Omit<ParameterCommand, keyof BaseCommand>;
+        } break;
+        default: {
+        } break;
     }
-    return vMap;
+    return {
+        command: Object.assign(baseCommand, cmdParams),
+        objectId: id,
+        time: startTime
+    } as TimelineEntry;
 };
 
 /**
@@ -55,8 +154,25 @@ const osbToIr = async (filepath: string) => {
         const line = lines[lineIdx].split(",");
         if (line[0].startsWith("//"))
             continue;
+
         switch (line[0]) {
-            
+            case "Sprite": {
+                const sprite = lineToSprite(line);
+                sb.objects.set(sprite.id, sprite);
+                while (true) {
+                    const command = CommandTypeMap[]
+                    sb.timeline.push({
+                        command: 
+                    })
+                    lineIdx += 1;
+                }
+            } break;
+            case "Animation": {
+
+            } break;
+            case "Sample": {
+
+            } break;
         }
     } while ((lineIdx += 1) < linesLen)
 };
--- src/ts/internalData.ts
+++ src/ts/internalData.ts
@@ -2,6 +2,18 @@ type Values<T> = T[keyof T];
 
 // Enums
 
+export const SbObjectType = {
+    Sprite: 0,
+    Animation: 1,
+    Sample: 2
+} as const;
+export type SbObjectType = Values<typeof SbObjectType>;
+export const SbObjectTypeMap: Record<string, SbObjectType> = {
+    "Sprite": SbObjectType.Sprite,
+    "Animation": SbObjectType.Animation,
+    "Sample": SbObjectType.Sample
+};
+
 export const Origin = {
     TopLeft: 0,
     Centre: 1,
@@ -15,6 +27,18 @@ export const Origin = {
     BottomRight: 9
 } as const;
 export type Origin = Values<typeof Origin>;
+export const OriginMap: Record<string, Origin> = {
+    "TopLeft": Origin.TopLeft,
+    "Centre": Origin.Centre,
+    "CentreLeft": Origin.CentreLeft,
+    "TopRight": Origin.TopRight,
+    "BottomCentre": Origin.BottomCentre,
+    "TopCentre": Origin.TopCentre,
+    "Custom": Origin.Custom,
+    "CentreRight": Origin.CentreRight,
+    "BottomLeft": Origin.BottomLeft,
+    "BottomRight": Origin.BottomRight
+};
 
 export const Easing = {
     Linear: 0,
@@ -54,6 +78,43 @@ export const Easing = {
     BounceInOut: 34,
 } as const;
 export type Easing = Values<typeof Easing>;
+export const EasingMap: Record<string, Easing> = {
+    "Linear": Easing.Linear,
+    "EasingOut": Easing.EasingOut,
+    "EasingIn": Easing.EasingIn,
+    "QuadIn": Easing.QuadIn,
+    "QuadOut": Easing.QuadOut,
+    "QuadInOut": Easing.QuadInOut,
+    "CubicIn": Easing.CubicIn,
+    "CubicOut": Easing.CubicOut,
+    "CubicInOut": Easing.CubicInOut,
+    "QuartIn": Easing.QuartIn,
+    "QuartOut": Easing.QuartOut,
+    "QuartInOut": Easing.QuartInOut,
+    "QuintIn": Easing.QuintIn,
+    "QuintOut": Easing.QuintOut,
+    "QuintInOut": Easing.QuintInOut,
+    "SineIn": Easing.SineIn,
+    "SineOut": Easing.SineOut,
+    "SineInOut": Easing.SineInOut,
+    "ExpoIn": Easing.ExpoIn,
+    "ExpoOut": Easing.ExpoOut,
+    "ExpoInOut": Easing.ExpoInOut,
+    "CircIn": Easing.CircIn,
+    "CircOut": Easing.CircOut,
+    "CircInOut": Easing.CircInOut,
+    "ElasticIn": Easing.ElasticIn,
+    "ElasticOut": Easing.ElasticOut,
+    "ElasticHalfOut": Easing.ElasticHalfOut,
+    "ElasticQuarterOut": Easing.ElasticQuarterOut,
+    "ElasticInOut": Easing.ElasticInOut,
+    "BackIn": Easing.BackIn,
+    "BackOut": Easing.BackOut,
+    "BackInOut": Easing.BackInOut,
+    "BounceIn": Easing.BounceIn,
+    "BounceOut": Easing.BounceOut,
+    "BounceInOut": Easing.BounceInOut
+};
 
 export const Layer = {
     Background: 0,
@@ -62,13 +123,19 @@ export const Layer = {
     Foreground: 3
 } as const;
 export type Layer = Values<typeof Layer>;
+export const LayerMap: Record<string, Layer> = {
+    "Background": Layer.Background,
+    "Fail": Layer.Fail,
+    "Pass": Layer.Pass,
+    "Foreground": Layer.Foreground
+};
 
 export const LoopType = {
     LoopOnce: 0,
     LoopForever: 1
 } as const;
 export type LoopType = Values<typeof LoopType>;
-export const LOOPTYPE_MAP: Record<string, LoopType> = {
+export const LoopTypeMap: Record<string, LoopType> = {
     "LoopOnce": LoopType.LoopOnce,
     "LoopForever": LoopType.LoopForever
 };
@@ -79,25 +146,25 @@ export const TriggerType = {
     Hitsound: 2
 } as const;
 export type TriggerType = Values<typeof TriggerType>;
-export const TRIGGERTYPE_MAP: Record<string, TriggerType> = {
+export const TriggerTypeMap: Record<string, TriggerType> = {
     "Passing": TriggerType.Passing,
     "Failing": TriggerType.Failing,
     // Hitsound will need regex to handle properly, not a string constant.
 };
 
 export const CommandType = {
-  Fade: 0,
-  Move: 1,
-  MoveX: 2,
-  MoveY: 3,
-  Scale: 4,
-  VectorScale: 5,
-  Rotate: 6,
-  Color: 7,
-  Parameter: 8
+    Fade: 0,
+    Move: 1,
+    MoveX: 2,
+    MoveY: 3,
+    Scale: 4,
+    VectorScale: 5,
+    Rotate: 6,
+    Color: 7,
+    Parameter: 8
 } as const;
 export type CommandType = Values<typeof CommandType>;
-export const COMMANDTYPE_MAP: Record<string, CommandType> = {
+export const CommandTypeMap: Record<string, CommandType> = {
     "F": CommandType.Fade,
     "M": CommandType.Move,
     "MX": CommandType.MoveX,
@@ -114,81 +181,163 @@ export interface Vec2 {
     y: number;
 }
 
-export interface BaseCommand {
-    type: string;
+
+class SimpleCommandArray {
+    private data: ArrayBuffer;
+    private types: Uint8Array;
+    private easings: Uint8Array;
+    private startTimes: Uint32Array;
+    private endTimes: Uint32Array;
+    private startOpacities: Float32Array;
+    private endOpacities: Float32Array;
+    private startXs: Int16Array;
+    private startYs: Int16Array;
+    private endXs: Int16Array;
+    private endYs: Int16Array;
+    private startScaleXs: Float32Array;
+    private startScaleYs: Float32Array;
+    private endScaleXs: Float32Array;
+    private endScaleYs: Float32Array;
+    private startRotations: Float32Array;
+    private endRotations: Float32Array;
+    private startRs: Uint8Array;
+    private startGs: Uint8Array;
+    private startBs: Uint8Array;
+    private endRs: Uint8Array;
+    private endGs: Uint8Array;
+    private endBs: Uint8Array;
+    private parameters: Uint8Array;
+    // TODO/optimization: probably slower, but could help with memory footprint,
+    //   gather all commands, then have a build(), or require the counts in the constructor,
+    //   and only allocate needed memory, maintain indirection tables for each array
+    constructor(count: number) {
+        const fields8Bit = 9;
+        const array8BitBytes = count * Int8Array.BYTES_PER_ELEMENT;
+        const fields16Bit = 4;
+        const array16BitBytes = count * Int16Array.BYTES_PER_ELEMENT;
+        const fields32Bit = 10;
+        const array32BitBytes = count * Int32Array.BYTES_PER_ELEMENT;
+        this.data = new ArrayBuffer(
+            fields8Bit * array8BitBytes +
+            fields16Bit * array16BitBytes +
+            fields32Bit * array32BitBytes
+        );
+        let offset = 0;
+        // type: uint8 (enum, 8 vals)
+        this.types = new Uint8Array(this.data, offset, count);
+        offset += array8BitBytes;
+        // easing: uint8 (enum, 35 vals)
+        this.easings = new Uint8Array(this.data, offset, count);
+        offset += array8BitBytes;
+        // startTime: uint32
+        this.startTimes = new Uint32Array(this.data, offset, count);
+        offset += array32BitBytes;
+        // endTime: uint32
+        this.endTimes = new Uint32Array(this.data, offset, count);
+        offset += array32BitBytes;
+        
+        // Fade:
+        //   startOpacity: float32
+        this.startOpacities = new Float32Array(this.data, offset, count);
+        offset += array32BitBytes;
+        //   endOpacity: float32
+        this.endOpacities = new Float32Array(this.data, offset, count);
+        offset += array32BitBytes;
+
+        // Move/MoveX/MoveY:
+        //   startX: int16(?)
+        this.startXs = new Int16Array(this.data, offset, count);
+        offset += array16BitBytes;
+        //   startY: int16(?)
+        this.startYs = new Int16Array(this.data, offset, count);
+        offset += array16BitBytes;
+        //   endX: int16(?)
+        this.endXs = new Int16Array(this.data, offset, count);
+        offset += array16BitBytes;
+        //   endY: int16(?)
+        this.endYs = new Int16Array(this.data, offset, count);
+        offset += array16BitBytes;
+        // Scale/VectorScale:
+        //   startScale(X): float32
+        this.startScaleXs = new Float32Array(this.data, offset, count);
+        offset += array32BitBytes;
+        //   startScaleY: float32
+        this.startScaleYs = new Float32Array(this.data, offset, count);
+        offset += array32BitBytes;
+        //   endScale(X): float32
+        this.endScaleXs = new Float32Array(this.data, offset, count);
+        offset += array32BitBytes;
+        //   endScaleY: float32
+        this.endScaleYs = new Float32Array(this.data, offset, count);
+        offset += array32BitBytes;
+        // Rotate:
+        //   startRotation: float32
+        this.startRotations = new Float32Array(this.data, offset, count);
+        offset += array32BitBytes;
+        //   endRotation: float32
+        this.endRotations = new Float32Array(this.data, offset, count);
+        offset += array32BitBytes;
+        // Color:
+        //   startR: uint8
+        this.startRs = new Uint8Array(this.data, offset, count);
+        offset += array8BitBytes;
+        //   startG: uint8
+        this.startGs = new Uint8Array(this.data, offset, count);
+        offset += array8BitBytes;
+        //   startB: uint8
+        this.startBs = new Uint8Array(this.data, offset, count);
+        offset += array8BitBytes;
+        //   endR: uint8
+        this.endRs = new Uint8Array(this.data, offset, count);
+        offset += array8BitBytes;
+        //   endG: uint8
+        this.endGs = new Uint8Array(this.data, offset, count);
+        offset += array8BitBytes;
+        //   endB: uint8
+        this.endBs = new Uint8Array(this.data, offset, count);
+        offset += array8BitBytes;
+        // Parameter:
+        //   params: uint8 (enum, 3 vals)
+        this.parameters = new Uint8Array(this.data, offset, count);
+        offset += array8BitBytes;
+    }
+}
+
+export type Command = {
+    type: CommandType;
     easing: Easing;
     startTime: number;
     endTime: number;
+    // Fade
+    startOpacity: number | undefined;
+    endOpacity: number | undefined;
+    // Move/MoveX/MoveY
+    startX: number | undefined;
+    startY: number | undefined;
+    endX: number | undefined;
+    endY: number | undefined;
+    // Scale/VectorScale (scale in vector is scaleX)
+    startScale: number | undefined;
+    startScaleY: number | undefined;
+    endScale: number | undefined;
+    endScaleY: number | undefined;
+    // Rotate
+    startRotation: number | undefined;
+    endRotation: number | undefined;
+    // Color
+    startR: number | undefined;
+    startG: number | undefined;
+    startB: number | undefined;
+    endR: number | undefined;
+    endG: number | undefined;
+    endB: number | undefined;
+    // Parameter
+    parameter: "H" | "V" | "A" | undefined;
 }
-
-export interface FadeCommand extends BaseCommand {
-    type: "F";
-    startOpacity: number;
-    endOpacity: number;
-}
-
-export interface MoveCommand extends BaseCommand {
-    type: "M";
-    startX: number;
-    startY: number;
-    endX: number;
-    endY: number;
-}
-
-export interface MoveXCommand extends BaseCommand {
-    type: "MX";
-    startX: number;
-    endX: number;
-}
-
-export interface MoveYCommand extends BaseCommand {
-    type: "MY";
-    startY: number;
-    endY: number;
-}
-
-export interface ScaleCommand extends BaseCommand {
-    type: "S";
-    startScale: number;
-    endScale: number;
-}
-
-export interface VectorScaleCommand extends BaseCommand {
-    type: "V";
-    startScaleX: number;
-    startScaleY: number;
-    endScaleX: number;
-    endScaleY: number;
-}
-
-export interface RotateCommand extends BaseCommand {
-    type: "R";
-    startRotation: number;
-    endRotation: number;
-}
-
-export interface ColorCommand extends BaseCommand {
-    type: "C";
-    startR: number;
-    startG: number;
-    startB: number;
-    endR: number;
-    endG: number;
-    endB: number;
-}
-
-export interface ParameterCommand extends BaseCommand {
-    type: "P";
-    parameter: "H" | "V" | "A";
-}
-
-type Command = FadeCommand | MoveCommand | MoveXCommand | MoveYCommand |
-    ScaleCommand | VectorScaleCommand | RotateCommand | ColorCommand |
-    ParameterCommand;
 
 export interface StoryboardObject {
     id: string;
-    type: "sprite" | "animation" | "audio";
+    type: SbObjectType;
     layer: Layer;
     origin: Origin;
     filepath: string;
